d1eaf7f86517256962ba0a1593f857f15667c697
rams <99rams99@gmail.com>
2022-09-28 12:11:48 -0500

    AWS Beanstalk for CICD Pipeline initial commit

---
+++ src/main/java/com/rams/aws/codepipeline/BeanstalkCodepipelineApplication.java
@@ -0,0 +1,13 @@
+package com.rams.aws.codepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstalkCodepipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanstalkCodepipelineApplication.class, args);
+	}
+
+}
